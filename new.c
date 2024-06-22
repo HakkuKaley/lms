8da4bdf980ecaa04780ed640a801ad3add1fcd1b
Hakku Kaley <limbo.aayush@gmail.com>
2024-06-22 12:50:29 +0545

    update

--- new.c
+++ new.c
@@ -20,70 +20,17 @@ struct student
 
 FILE *fp;
 
-void startapp()
-{
-	int c;
-	while(1)
-	{
-		system("cls");
-		printf("========================================================\n");
-		printf("\n		LIBRARY MANAGEMENT SYSTEM\n");
-		printf("\n========================================================");
-		printf("\n\nPlease Login to continue...\n\n");
-		printf("1.  Login.\n");
-		printf("2.  Register.\n");
-		printf("3.  Exit.\n");
-		printf("__________________________________________________________\n\n");
-		printf("Enter your choice.\n");
-		scanf("%d", &c);
-		switch (c)
-		{
-			case 1:
-				login();
-				break;
-			case 2:
-				
-				break;
-			case 3:
-				system("cls");
-				exit(0);
-			default:
-				printf("Invalid choice");
-				exit(0);
-		}
-	}
-}
-
 int login()
 {
 	system("cls");
-	int y, a=5;
+	int y;
 	printf("Press Y\n");
 	scanf("%d", &y);
-	if(y==a)
+	if(y=="Y")
 	{
-		printf("Authentication Successfull!\n");
-		printf("\nPress any key to continue.\n");
-		getch();
-		menu();
+		return 1;
 	}
-	else
-	{
-		printf("**Incorrect username or password**\n");
-		printf("\nPress any key to try again.\n");
-		getch();
-		return 0;
-	}
-}
-
-void addbook()
-{
-	printf("Enter Book ID.\n");
-	scanf("%d", &b.id);
-	fflush("stdin");
-	printf("Enter the bookname.\n");
-	scanf("%s", &b.bookname);
-	
+	getch();
 }
 
 void menu()
@@ -107,7 +54,7 @@ void menu()
 		switch (c)
 		{
 			case 1:
-				addbook();
+				login();
 				break;
 			case 2:
 				break;
@@ -126,6 +73,25 @@ void menu()
 		}
 	}
 	
+}
+
+void startapp()
+{
+	int c;
+	while(1)
+	{
+		system("cls");
+		printf("========================================================\n");
+		printf("\n		LIBRARY MANAGEMENT SYSTEM\n");
+		printf("\n========================================================");
+		printf("\n\nPlease Login to continue...\n\n");
+		printf("1.  Login.\n");
+		printf("2.  Register.\n");
+		printf("3.  Exit.\n");
+		printf("__________________________________________________________\n\n");
+		printf("Enter your choice.\n");
+		scanf("%d", &c);
+	}
 }
 
 int main()
